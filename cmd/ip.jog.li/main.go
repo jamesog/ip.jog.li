23819a5945bad666199ac9fa6218999723023db4
James O'Gorman <james@netinertia.co.uk>
2018-01-28 19:34:13 +0000

    Add an ip.jog.li command with a Dockerfile
    This command runs the GAE package in a similar way to GAE.
    
    A Dockerfile and Makefile are added for building an image and uploading
    to Google Container Registry.

---
+++ cmd/ip.jog.li/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	_ "github.com/jamesog/ip.jog.li"
+)
+
+func main() {
+	s := &http.Server{
+		Addr:         ":8000",
+		Handler:      nil,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+	log.Fatal(s.ListenAndServe())
+}
