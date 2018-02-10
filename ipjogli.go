97314a773dd7076a14c1cc34529193e5e05cb850
James O'Gorman <james@netinertia.co.uk>
2018-02-10 15:51:13 +0000

    Correctly handle IP on GAE vs. standalone
    On App Engine the `RemoteAddr` doesn't have a port. Running
    `net.SplitHostPort()` on an IPv6 address in GAE causes an error because
    there are too many colons and the IPv6 address isn't surrounded by `[]`.
    
    This change looks for an App Engine header in the request and if
    present, use uses `RemoteAddr` as-is, otherwise `SplitHostPort()` is
    used.

--- ipjogli.go
+++ ipjogli.go
@@ -11,10 +11,16 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	addr, _, err := net.SplitHostPort(r.RemoteAddr)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+	var addr string
+	if _, ok := r.Header["X-Appengine-Remote-Addr"]; ok {
+		addr = r.RemoteAddr
+	} else {
+		var err error
+		addr, _, err = net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	}
 	fmt.Fprintf(w, addr+"\n")
 }
