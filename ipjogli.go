e6c8419e88eff2b173377f235cde055b795745bf
James O'Gorman <james@netinertia.co.uk>
2019-04-27 16:56:23 +0100

    Add support for X-Forwarded-For
    When deploying the server behind a proxy using the request's remote
    address doesn't help as it's likely some internal IP. In these cases the
    X-Forwarded-For header is added.
    
    This adds support for parsing this header, always using the last
    globally-routable address in the chain as the presumed public IP of the
    client.
    
    Some extra parsing of the IP needs to be done because Go's net library
    has a function called IsGlobalUnicast() which is a bit of a lie: it
    reports addresses as being global even if they're private addresses. A
    reasonably comprehensive list of non-routed networks is evaluated
    against the proxy address to determine the last routable address.

--- ipjogli.go
+++ ipjogli.go
@@ -15,12 +15,45 @@ func init() {
 	http.HandleFunc("/info", infoHandler)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+// remoteAddr reads an HTTP request and returns an IP (in string form).
+func remoteAddr(r *http.Request) string {
+	// Attempt to get the IP from the X-Forwarded-For header.
+	// If that's empty then use the request's remote address.
+	// N.B. if there are multiple X-Forwarded-For headers (meaning the user
+	// went through multiple proxies) we use the last globally-routable
+	// address.
+	xff := r.Header.Get("X-Forwarded-For")
+	if xff != "" {
+		addrs := strings.Split(xff, ",")
+		// Walk backwards through the XFF addresses.
+		// Typically a user of this library wants to know what they appear as
+		// to a remote server so we don't want the first egress IP, rather
+		// the last possible proxy that isn't internal to the network of this
+		// server.
+		for i := len(addrs) - 1; i >= 0; i-- {
+			ip := net.ParseIP(strings.TrimSpace(addrs[i]))
+			// If this is a globally-routable address, assume it's the last
+			// known ingress and therefore how the request is seen by HTTP
+			// servers.
+			if ip.IsGlobalUnicast() && isRoutableAddr(ip) {
+				return ip.String()
+			}
+		}
+	}
+
+	// Parse the HTTP connection address
 	addr, _, err := net.SplitHostPort(r.RemoteAddr)
 	if err != nil {
 		// Assume that this is App Engine and thus there was no port
 		addr = r.RemoteAddr
 	}
+	return addr
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Attempt to get the IP from the X-Forwarded-For header.
+	// If that's empty then use the request's remote address.
+	addr := remoteAddr(r)
 	fmt.Fprintf(w, addr+"\n")
 }
 
@@ -76,16 +109,12 @@ func whoisIP(r *http.Request, ip string) (ipInfo, error) {
 }
 
 func infoHandler(w http.ResponseWriter, r *http.Request) {
-	addr, _, err := net.SplitHostPort(r.RemoteAddr)
-	if err != nil {
-		// Assume that this is App Engine and thus there was no port
-		addr = r.RemoteAddr
-	}
+	addr := remoteAddr(r)
 
 	// Attempt to fetch from memcache
 	var info ipInfo
-	info, err = mcGet(r, addr)
-	if err != nil {
+	var err error
+	if info, err = mcGet(r, addr); err != nil {
 		// Memcache miss, so fetch from Team Cymru and save to memcache
 		info, err = whoisIP(r, addr)
 		if err != nil {
---
+++ net.go
@@ -0,0 +1,39 @@
+package ipjogli
+
+import "net"
+
+// Helpers for evaluating an IP address
+
+func init() {
+	nonGlobalNets = make([]net.IPNet, 0, len(nonGlobalCIDRs))
+	for _, cidr := range nonGlobalCIDRs {
+		_, net, _ := net.ParseCIDR(cidr)
+		nonGlobalNets = append(nonGlobalNets, *net)
+	}
+}
+
+// The list of CIDRs net.IP.IsGlobaUnicast() incorrectly reports as global addresses
+var nonGlobalCIDRs = []string{
+	"2001:db8::/32",   // RFC3849 Documentation
+	"fc00::/7",        // RFC4193 Unique Local Addressing
+	"10.0.0.0/8",      // RFC1918 Private
+	"100.64.0.0/10",   // RFC6598 Private (CGNAT)
+	"172.16.0.0/12",   // RFC1918 Private
+	"192.0.0.0/24",    // RFC6890 Private
+	"192.0.2.0/24",    // RFC5737 Documentation
+	"192.168.0.0/16",  // RFC1918 Private
+	"198.18.0.0/15",   // RFC6815 Private
+	"198.51.100.0/24", // RFC5737 Documentation
+	"203.0.113.0/24",  // RFC5737 Documentation
+}
+
+var nonGlobalNets []net.IPNet
+
+func isRoutableAddr(ip net.IP) bool {
+	for _, net := range nonGlobalNets {
+		if net.Contains(ip) {
+			return false
+		}
+	}
+	return true
+}
