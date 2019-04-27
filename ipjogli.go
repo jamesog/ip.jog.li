2f4cdbb56430036e97bea14b0996a74c1dd469f6
James O'Gorman <james@netinertia.co.uk>
2019-04-27 17:21:54 +0100

    Replace whoisIP with iptoasn package
    I created the iptoasn package (github.com/jamesog/iptoasn) to be a more
    general utility library. It uses the DNS interface to Team Cymru,
    whereas the implementation in this package uses the TCP WHOIS service.
    
    DNS is generally faster and cached in various places, so I've ripped out
    the memcache code from this package. Memcache was also only supported in
    first-gen runtimes in App Engine; second-gen required using
    externally-hosted Redis or another GCP product.
    
    I may add caching back at a later point but for now the DNS service is
    generally good enough that it's not required and shouldn't hurt Team
    Cymru's service if someone decides to abuse this service.

--- ipjogli.go
+++ ipjogli.go
@@ -1,13 +1,14 @@
 package ipjogli
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net"
 	"net/http"
-	"strconv"
 	"strings"
+
+	"github.com/jamesog/iptoasn"
 )
 
 func init() {
@@ -57,75 +58,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, addr+"\n")
 }
 
-type ipInfo struct {
-	IP        string `json:"ip"`
-	BGPPrefix string `json:"bgp_prefix"`
-	AS        int    `json:"as"`
-	ASName    string `json:"as_name"`
-	Country   string `json:"country"`
-	Registry  string `json:"registry"`
-}
-
-var whoisAddr = "whois.cymru.com:43"
-
-func whoisIP(r *http.Request, ip string) (ipInfo, error) {
-	conn, err := newWhoisConn(r)
-	if err != nil {
-		return ipInfo{}, err
-	}
-
-	// -v enable all fields
-	// -f disable header
-	// Order matters!
-	fmt.Fprintf(conn, "-v -f %s\n", ip)
-	res, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil {
-		return ipInfo{}, nil
-	}
-	conn.Close()
-
-	fields := strings.Split(res, "|")
-	for i, _ := range fields {
-		fields[i] = strings.TrimSpace(fields[i])
-	}
-
-	// Convert the AS to an integer
-	// The AS may be returned as "NA", e.g. for non-global IPs
-	as, err := strconv.Atoi(fields[0])
-	if err != nil && fields[0] != "NA" {
-		return ipInfo{}, err
-	}
-
-	info := ipInfo{
-		IP:        fields[1],
-		BGPPrefix: fields[2],
-		AS:        as,
-		ASName:    fields[6],
-		Country:   fields[3],
-		Registry:  fields[4],
-	}
-
-	return info, nil
-}
-
 func infoHandler(w http.ResponseWriter, r *http.Request) {
 	addr := remoteAddr(r)
 
 	// Attempt to fetch from memcache
-	var info ipInfo
-	var err error
-	if info, err = mcGet(r, addr); err != nil {
-		// Memcache miss, so fetch from Team Cymru and save to memcache
-		info, err = whoisIP(r, addr)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		mcSet(r, info)
+	info, err := iptoasn.LookupIP(addr)
+	if err != nil {
+		log.Printf("lookup error: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	i, err := json.Marshal(info)
 	if err != nil {
+		log.Printf("JSON marshal error: %v", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
