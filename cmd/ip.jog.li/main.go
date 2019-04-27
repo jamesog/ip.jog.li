09fa42715fcfb96c6a0c23a91478c4fea8d8ec1f
James O'Gorman <james@netinertia.co.uk>
2019-04-27 15:29:19 +0100

    cmd/ip.jog.li: Read PORT from the environment
    The port the service listens on was hard-coded before. It now supports
    taking reading `PORT` from the environment to be compatible with Google
    Cloud Run.
    
    The default port is still 8080 if the environment variable is unset.

--- cmd/ip.jog.li/main.go
+++ cmd/ip.jog.li/main.go
@@ -5,14 +5,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	_ "github.com/jamesog/ip.jog.li"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	s := &http.Server{
-		Addr:         ":8000",
+		Addr:         ":" + port,
 		Handler:      nil,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
