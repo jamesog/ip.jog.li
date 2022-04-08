2ab34b1cbc390d36882005d86784cfd64eb959c5
James O'Gorman <james@netinertia.co.uk>
2022-04-08 17:49:12 +0100

    Fix tests
    The tests have been broken since commit
    2f4cdbb56430036e97bea14b0996a74c1dd469f6, when the IP lookup
    functionality was moved to a separate package.
    
    The tests for /info have been removed as they're not easy to mock, as
    the iptoasn package uses DNS lookups instead of whois, and you can't
    tell Go's DNS client code to use a specified resolver; netgo only uses
    /etc/resolv.conf.
    
    The / code now tests that XFF was parsed properly, or on Fly.io, that it
    used the Fly-Client-IP header.

--- ipjogli_test.go
+++ ipjogli_test.go
@@ -1,85 +1,57 @@
 package ipjogli
 
 import (
-	"bytes"
-	"encoding/json"
 	"io/ioutil"
-	"net"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
-// This fakes the Team Cymru WHOIS service
-func testWhoisServer(t *testing.T) {
-	l, err := net.Listen("tcp", whoisAddr)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer l.Close()
-
-	info := bytes.NewBufferString("NA      | 192.0.2.1        | NA                  |    | other    |            | NA\n")
-
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			t.Fatal(err)
-		}
-		go func(c net.Conn) {
-			c.Write(info.Bytes())
-			c.Close()
-		}(conn)
-	}
-}
-
 func TestIP(t *testing.T) {
-	req := httptest.NewRequest("GET", "/", nil)
-	w := httptest.NewRecorder()
-	handler(w, req)
-
-	resp := w.Result()
-	body, _ := ioutil.ReadAll(resp.Body)
+	tests := map[string]struct {
+		headers map[string]string
+		env     map[string]string
+	}{
+		"direct": {headers: nil},
+		"xff":    {headers: map[string]string{"X-Forwarded-For": "10.0.0.1, 192.0.2.1"}},
+		"fly": {
+			headers: map[string]string{
+				"Fly-Client-IP":   "192.0.2.1",
+				"X-Forwarded-For": "10.0.0.1, 192.0.2.1, 198.51.100.80",
+			},
+			env: map[string]string{"FLY_APP_NAME": "test"},
+		},
+	}
 	expected := "192.0.2.1\n"
 
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("expected status %v, got %v", http.StatusOK, resp.StatusCode)
-	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			req := httptest.NewRequest("GET", "/", nil)
+			for hdr, val := range tt.headers {
+				req.Header.Set(hdr, val)
+			}
+			for env, val := range tt.env {
+				os.Setenv(env, val)
+			}
+			w := httptest.NewRecorder()
+			handler(w, req)
 
-	if string(body) != expected {
-		t.Errorf("expected body %q, got %q", expected, string(body))
-	}
-}
+			resp := w.Result()
+			body, _ := ioutil.ReadAll(resp.Body)
 
-func TestInfo(t *testing.T) {
-	whoisAddr = "[::1]:10043"
+			if resp.StatusCode != http.StatusOK {
+				t.Errorf("expected status %v, got %v", http.StatusOK, resp.StatusCode)
+			}
 
-	go testWhoisServer(t)
+			if string(body) != expected {
+				t.Errorf("expected body %q, got %q", expected, string(body))
+			}
 
-	req := httptest.NewRequest("GET", "/info", nil)
-	w := httptest.NewRecorder()
-	infoHandler(w, req)
-
-	info := ipInfo{
-		IP:        "192.0.2.1",
-		BGPPrefix: "NA",
-		AS:        0,
-		ASName:    "NA",
-		Country:   "",
-		Registry:  "other",
-	}
-	want, err := json.Marshal(info)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	resp := w.Result()
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("wanted status %v, got %v", http.StatusOK, resp.StatusCode)
-	}
-
-	if string(body) != string(want) {
-		t.Errorf("wanted body %q, got %q", string(want), string(body))
+			// Clean up any environment variables we set to not affect other tests
+			for env := range tt.env {
+				os.Unsetenv(env)
+			}
+		})
 	}
 }
